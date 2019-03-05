9c6468f2127dc71f910cc3b63db2ef9572479435
Rassani Mehdi <mehdi-kaso@mehdi-kaso1.local>
2019-03-05 16:02:38 +0900

    feature/ffmpeg-link : link fixed

--- install_scripts/install.sh
+++ install_scripts/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-64bit-static.tar.xz
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
 
 mkdir ffmpeg-64bit-static-build
 
-tar xf ffmpeg-git-64bit-static.tar.xz -C ffmpeg-64bit-static-build --strip-components 1
+tar xf ffmpeg-release-amd64-static.tar.xz -C ffmpeg-64bit-static-build --strip-components 1
 
 mv ffmpeg-64bit-static-build/ ffmpeg/
 
-rm ffmpeg-git-64bit-static.tar.xz
+rm ffmpeg-release-amd64-static.tar.xz
 
 pip install -r /tmp/requirements.txt
